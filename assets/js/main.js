f28f18ec52c3a7561c104da3edc888bb8fc58095
zolveuf <zolveuf@gmail.com>
2024-12-07 12:33:18 +0100

    okokk

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,16 +80,3 @@ const handleMenuToggle = () => {
 window.addEventListener('scroll', handleScroll);
 toggle.addEventListener('click', handleMenuToggle);
 
-// Scroll in animations
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show')
-        }
-    });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
